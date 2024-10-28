0bd0486e72cc0a9115cb9db0ed49c32a53d50a6d
unknown <zakariaaoladaissa@gmail.com>
2024-10-28 17:07:14 +0000

    added save and findbyid

--- src/main/java/com/example/carRental/controllers/CarsController.java
+++ src/main/java/com/example/carRental/controllers/CarsController.java
@@ -3,12 +3,13 @@ package com.example.carRental.controllers;
 import com.example.carRental.models.Car;
 import com.example.carRental.services.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/cars")
@@ -19,5 +20,17 @@ public class CarsController {
     @GetMapping
     public ResponseEntity<List<Car>> getAllCars(){
         return ResponseEntity.ok(carService.getAllCars());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Car> getCarById(@PathVariable Integer id) {
+        return carService.getById(id)
+                .map(ResponseEntity::ok) // If car is found, return it with 200 OK
+                .orElseGet(() -> ResponseEntity.notFound().build()); // If not found, return 404 Not Found
+    }
+
+    @PostMapping
+    public ResponseEntity<Car> addCar(@RequestBody Car car){
+        return ResponseEntity.status(HttpStatus.CREATED).body(carService.saveCar(car));
     }
 }
--- src/main/java/com/example/carRental/services/CarService.java
+++ src/main/java/com/example/carRental/services/CarService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+
 @Service
 public class CarService {
 
@@ -14,5 +16,14 @@ public class CarService {
 
     public List<Car> getAllCars(){
         return carRepository.findAll();
+    }
+
+    //Optional because result might be null
+    public Optional<Car> getById(int id){
+        return carRepository.findById(id);
+    }
+
+    public Car saveCar(Car car){
+       return carRepository.save(car);
     }
 }
